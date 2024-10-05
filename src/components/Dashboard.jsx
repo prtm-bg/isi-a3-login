b52f460917ebe50109de159adc6233c6064ce8d2
prtm-bg <prtm1609@gmail.com>
2024-10-05 20:04:29 +0530

    Initial commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to the Main Page</h1>
+      <h2> See! The Login Works  ;) </h2>
+    </div>
+  );
+};
+
+export default Dashboard;
---
+++ src/components/Login.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    setError('');
+
+    if (!username || !password) {
+      setError('Please fill out both fields');
+      return;
+    }
+
+    try {
+      const response = await axios.get(`http://152.67.176.72:8081/userauth?username=${username}&password=${password}`);
+
+      if (response.data == true) {
+        // Redirect to dashboard on successful login
+        navigate('/dashboard');
+      } else {
+        // Show error message if credentials are incorrect
+        setError('Invalid username or password');
+      }
+    } catch (err) {
+      console.error(err);
+      setError('An error occurred while trying to log in');
+    }
+  };
+
+  return (
+    <div className='logindiv'>
+      <form className='loginform' onSubmit={handleLogin}>
+        <h2>Login</h2>
+        {error && <p style={{ color: 'red' }}>{error}</p>}
+        <div className='logininputdiv'>
+          <label>Username</label>
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className='logininputdiv'>
+          <label>Password</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
